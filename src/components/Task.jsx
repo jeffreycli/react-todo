2ed46c1e12e7c2e7312aaf5ddad174fb53078a01
Jeffrey Li <jeffrey.li@dda.kyocera.com>
2020-01-24 17:03:39 -0800

    Completed tasks are checked

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Task(props) {
     return(
         <div>
-            <input type="checkbox" onClick={() => props.switchList(props.id)}></input>
+            <input type="checkbox" onClick={() => props.switchList(props.id)} defaultChecked={props.completed}></input>
             <span>{props.taskText}</span>
         </div>
     )
